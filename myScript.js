36c4814e00ea99a0713f6c343e984bee12bc6046
storm-keith <dejitoloyla.00@gmail.com>
2021-02-10 19:00:51 +0800

    web-dev

---
+++ myScript.js
@@ -0,0 +1,34 @@
+// function myFunction(){
+//     document.getElementById("demo").innerHTML="Paragraphed changed";
+// }
+
+function addBy(){
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function subtractBy(){
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = num1 - num2;
+
+}
+
+function multiplyBy(){
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+
+}
+
+function divideBy(){
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+
+}
+
+function resetBy(){
+    
+}
